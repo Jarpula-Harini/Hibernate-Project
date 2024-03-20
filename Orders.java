7fb90776edb296e17ca9c764ffa6487d4e69527e
Jarpula-Harini <102748555+Jarpula-Harini@users.noreply.github.com>
2024-03-20 13:45:24 +0530

    Add files via upload

---
+++ Orders.java
@@ -0,0 +1,138 @@
+package com.shopping.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+public class Orders {
+	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String orderId;
+
+	private LocalDate orderDate;
+
+	private double totalAmount;
+
+	private String shippingAddress;
+
+	private String paymentMethod;
+
+	private String orderStatus;
+	
+	@ColumnDefault("'A'")
+	private char status;
+
+	@ManyToOne
+	private User user;
+	
+	public double totalAmounts(){
+		OrderDetail orderDetail = new OrderDetail();
+		Product product = new Product();
+		totalAmount = product.getproductPrice()*orderDetail.getQuantity();
+		return totalAmount;
+		}
+
+
+
+	// Default Constructor
+	public Orders() {}
+	
+	
+
+	public Orders(String orderId, LocalDate orderDate, double totalAmount, String shippingAddress, String paymentMethod,
+			String orderStatus, char status, User user) {
+		super();
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.totalAmount = totalAmount;
+		this.shippingAddress = shippingAddress;
+		this.paymentMethod = paymentMethod;
+		this.orderStatus = orderStatus;
+		this.status = 'A';
+		this.user = user;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Orders [orderId=" + orderId + ", orderDate=" + orderDate + ", totalAmount=" + totalAmount
+				+ ", shippingAddress=" + shippingAddress + ", paymentMethod=" + paymentMethod + ", orderStatus="
+				+ orderStatus + ", status=" + status + ", user=" + user + "]";
+	}
+	
+
+	
+
+}
---
+++ Review.java
@@ -0,0 +1,89 @@
+package com.shopping.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+public class Review {
+
+	@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private String reviewId;
+
+	@Positive(message = "Invalid rating. Value should be positive.")
+	private double rating;
+
+	private String comments;
+
+	@ColumnDefault("'A'")
+	private char status;
+
+	@ManyToOne
+	private User user;
+
+	// Default Constructor
+	public Review() {
+	}
+
+	public Review(String reviewId, double rating,String comments, char status, User user) {
+		super();
+		this.reviewId = reviewId;
+		this.rating = rating;
+		this.comments = comments;
+		this.status = status;
+		this.user = user;
+	}
+
+	public String getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [reviewId=" + reviewId + ", rating=" + rating + ", comments=" + comments + ", status=" + status
+				+ ", user=" + user + "]";
+	}
+	
+
+}
